eb8dfc47c6fbe94c17f12a3cd92b3fe93f65b49d
Jesse Gilbride <jessegilbride.dev@gmail.com>
2020-12-13 18:18:50 -0500

    update rendering function

--- app.js
+++ app.js
@@ -4,42 +4,61 @@ const store = {
   errorIngredientNotFound: "Sorry, we can't find a recipe based on your search. Please try again.",
   errorNoServerResonse: "Seems like we're having trouble contacting the service that provides recipes. Check your internet connection and try again. Thanks!",
   errorPointsQuotaExceeded: "Unfortunately you've reached the daily quota of allowed searches. Please come back tomorrow.",
-  searchNotInitiated: true,
-  resultsDisplayed: false,
-  recipeShown: false
+  displaySearch: true,
+  displayResults: false,
+  displayRecipe: false
 };
 
 /********** TEMPLATE GENERATION **********/
+// >> generators create dynamic HTML <<
 
 
+/********** API CALLS **********/
+
+getResults(ingredient) {}
 
 /********** APP RENDERING **********/
+// >> render says which screen to show <<
 
 function render() {
-  /* could this conditional be better handled with a case-switch? */
-  if (store.searchNotInitiated === true) {
-    // hide the results and recipe screens
-    // show the search screen
-  } elseif (store.resultsDisplayed === true) {
-    // hide the search and recipe screens
-    // show the results screen
-  } elseif (store.recipeShown === true) {
-    // hide the search and results screens
-    // show the recipe screen
+  if (store.displaySearch === true) {
+    $('#js-search-screen').show();
+    $('#js-results-screen').hide();
+    $('#js-recipe-screen').hide();
+  } else if (store.displayResults === true) {
+    $('#js-search-screen').hide();
+    $('#js-results-screen').show();
+    $('#js-recipe-screen').hide();
+  } else if (store.displayRecipe === true) {
+    $('#js-search-screen').hide();
+    $('#js-results-screen').hide();
+    $('#js-recipe-screen').show();
   }
 }
 
 /********** EVENT HANDLERS **********/
+// >> event handlers change state <<
+
+function handleExportRecipe() {}
+
+function handleSearchResultClicked() {}
+
+function handleBackToResults() {}
+
+function handleBackToSearch() {}
 
 function handleSearchForm() {
   $('main').on('submit', '.js-recipe-search-form', function(event) {
     event.preventDefault();
-    // do stuff
     const searchTerm = $('#js-search-input').val();
+
     getResults(searchTerm);
-    // change store state
-    // call render()
-  }
+
+    // change screen states, re-render
+    store.displaySearch = false;
+    store.displayResults = true;
+    render();
+  })
 }
 
 function handleApp() {
